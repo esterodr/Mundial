da7b26372fdc84fb2095df95d1b50292e7390052
Esteban Rodriguez <esteban.rgz@gmail.com>
2022-11-24 13:05:53 -0300

    uruguay corea

--- nuevo.R
+++ nuevo.R
@@ -1,39 +1,50 @@
 library(tidyverse)
 
-# load("resultados_0.Rda")
-# 
-# df <- prob_grupos |> select(id,P1) |> 
-#   mutate(Fecha=as.Date("2022-11-19"))
-# 
-# load("resultados_1.Rda")
-# 
-# df2 <- prob_grupos |> select(id,P1) |> 
-#   mutate(Fecha=as.Date("2022-11-20"))
-# 
-# df <- rbind(df,df2)
-# 
-# load("resultados_4.Rda")
-# 
-# df2 <- prob_grupos |> select(id,P1) |> 
-#   mutate(Fecha=as.Date("2022-11-21"))
-# 
-# df <- rbind(df,df2)
-# 
-# load("resultados_7.Rda")
-# 
-# df2 <- prob_grupos |> select(id,P1) |> 
-#   mutate(Fecha=as.Date("2022-11-22"))
-# 
-# df <- rbind(df,df2)
-# 
-# load("resultados.Rda")
-# 
-# df2 <- prob_grupos |> select(id,P1) |> 
-#   mutate(Fecha=as.Date("2022-11-23"))
-# 
-# df <- rbind(df,df2)
-# 
-# rm(df2,prob_grupos,simulaciones)
+load("resultados_0.Rda")
+
+df <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-19"))
+
+load("resultados_1.Rda")
+
+df2 <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-20"))
+
+df <- rbind(df,df2)
+
+load("resultados_4.Rda")
+
+df2 <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-21"))
+
+df <- rbind(df,df2)
+
+load("resultados_7.Rda")
+
+df2 <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-22"))
+
+df <- rbind(df,df2)
+
+load("resultados_12.Rda")
+
+df2 <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-23"))
+
+df <- rbind(df,df2)
+
+load("resultados.Rda")
+
+df2 <- prob_grupos |> select(id,P1) |>
+  mutate(Fecha=as.Date("2022-11-24"))
+
+df <- rbind(df,df2)
+
+rm(df2,prob_grupos,simulaciones)
+
+save(df,file = "evolucion2.Rda")
+
+rm(list=ls())
 
 load("resultados_0.Rda")
 
@@ -65,11 +76,19 @@ df2 <- prob_grupos |> select(id) |>
 
 df <- rbind(df,df2)
 
-load("resultados.Rda")
+load("resultados_12.Rda")
 
 df2 <- prob_grupos |> select(id) |>
   mutate(Posición=seq(1,32,1),
          Fecha=as.Date("2022-11-23"))
+
+df <- rbind(df,df2)
+
+load("resultados.Rda")
+
+df2 <- prob_grupos |> select(id) |>
+  mutate(Posición=seq(1,32,1),
+         Fecha=as.Date("2022-11-24"))
 
 df <- rbind(df,df2)
 
@@ -78,29 +97,3 @@ rm(df2,prob_grupos,simulaciones)
 save(df,file = "evolucion.Rda")
 
 
-df |> mutate(Posición=-1*Posición) |> 
-  ggplot(aes(x=Fecha,y=Posición,color=id)) +
-  geom_line(size=1) +
-  geom_point(aes(fill=id),shape=21,size=2,color="black") +
-  scale_y_continuous(breaks = unique(-1*df$Posición),
-                     labels = df$id[df$Fecha==min(df$Fecha)],
-                     sec.axis = sec_axis(~ . * 1,breaks = unique(-1*df$Posición),
-                                         labels = df$id[df$Fecha==max(df$Fecha)])) +
-  scale_x_continuous(breaks = c(0,1,2)) +
-  labs(x="",y="") +
-  theme(legend.position = "null",
-        panel.background = element_blank(),
-        axis.ticks = element_blank())
-
-# df |> ggplot(aes(x=Fecha,y=P1,color=id)) +
-#   geom_line(size=1) +
-#   geom_point(aes(fill=id),shape=21,size=2,color="black") +
-#   scale_y_continuous(breaks = unique(df$P1[df$Fecha==min(df$Fecha)]),
-#                      labels = df$id[df$Fecha==min(df$Fecha)],
-#                      sec.axis = sec_axis(~ . * 1,breaks = unique(df$P1[df$Fecha==max(df$Fecha)]),
-#                                          labels = df$id[df$Fecha==max(df$Fecha)])) +
-#   scale_x_continuous(breaks = c(0,1,2)) +
-#   labs(x="",y="") +
-#   theme(legend.position = "null",
-#         panel.background = element_blank(),
-#         axis.ticks = element_blank())
